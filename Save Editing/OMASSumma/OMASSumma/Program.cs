a261f30c369339f96ea2e7505d0aa27e39bd19ac
MarkH221 <MarkH221@users.noreply.github.com>
2017-07-18 11:08:24 +0100

    Added 'of mice and sand checksum fixer' source.

---
+++ Save Editing/OMASSumma/OMASSumma/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace OMASSumma
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (!File.Exists(args[0])) return;
+                UInt32 sum = 0;
+                using (var o = new FileStream(args[0], FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
+                {
+                    o.Position = 0x10;
+                    byte[] buf = new byte[4];
+                    for (int i = 0; i < o.Length - 0x10; i += 4)
+                    {
+                        o.Read(buf, 0, 4);
+                        sum += BitConverter.ToUInt32(buf, 0);
+                    }
+                    Console.WriteLine("New Checksum: {0}", sum.ToString("X8"));
+                    o.Position = 0xc;
+                    o.Write(BitConverter.GetBytes(sum), 0, 4);
+                }
+                Console.Beep();
+            }
+            catch (Exception)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
